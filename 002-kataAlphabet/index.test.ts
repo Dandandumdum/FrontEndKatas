60f263894f04bd6855bafcc2f6bf2966466be5ea
Daniel Dumville <danieldumville@gmail.com>
2021-08-05 10:02:33 +0200

    Added additional tests

--- 002-kataAlphabet/index.test.ts
+++ 002-kataAlphabet/index.test.ts
@@ -3,6 +3,12 @@ import {alphabetSoup} from "./index"
 test('dcba should be abcd', ()=>{
     expect(alphabetSoup("dcba")).toBe("abcd");
 });
+test('hello should be elloh ', ()=>{
+    expect(alphabetSoup("hello")).toBe("ehllo");
+});
+test('javascript should be aacijprstv', ()=>{
+    expect(alphabetSoup("javascript")).toBe("aacijprstv");
+});
 
 test('dcba should not be dbca',() =>{
     expect(alphabetSoup('dcba')).not.toBe("dbca")
